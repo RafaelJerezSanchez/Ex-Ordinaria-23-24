72835d2f249ec24882a244ff554d33c545cf3c01
RafaelJerezSanchez <rafaeljerezsanchez@gmail.com>
2024-06-12 15:27:55 +0000

    commit 2

--- rjsJardineriaHLC/rjsdockerfiles/rjsbase/rjsscripts/rjsstart.sh
+++ rjsJardineriaHLC/rjsdockerfiles/rjsbase/rjsscripts/rjsstart.sh
@@ -3,15 +3,11 @@ set -e
 
 ping -c 4 172.10.10.10
 
-# Get environmetn
 REPO=$REPO
 DB_HOST=$DB_HOST
 DB_PORT=$DB_PORT
 
 cd /root/api
-pwd
-pwd
-pwd
 rm -rf .git
 
 git config --global init.defaultBranch master
@@ -21,12 +17,10 @@ git remote add origin ${REPO}
 git branch -m master
 git pull origin master
 
-# Replace values in env. file
 sed -i "s/127.0.0.1/${DB_HOST}/g" .env
 sed -i "s/5433/${DB_PORT}/g" .env
 cat .env
 
-# This takes a while
 npm install -g npm-check-updates
 npm install --force
 
@@ -38,5 +32,3 @@ npm audit fix --force
 
 
 npm run start dev
-
-exec "$@"
